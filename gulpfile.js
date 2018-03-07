dd50778a858042a71f06f8298a34bfd99d47f7bd
skhodparast <sajjad4263@gmail.com>
2018-03-07 17:45:25 +0330

    using @import and _variable

--- gulpfile.js
+++ gulpfile.js
@@ -26,40 +26,10 @@ gulp.task('reload_watch', function(){
   gulp.start('watch')
 })
 
-// var condition = true;
-var condition = function () {
-  if( fileExists('_dest/css/main.css'))
-    return true;
-};
-
-gulp.task('removeFiles', function(){
-  return gulp.src('_dest/css/main.css')
-  .pipe(gulpif(condition, removeFiles()))
-})
-
-// gulp.task('removeFiles' , function(){
-//   fileExists('_dest/css/main.css').then(exists => {
-//   console.log(exists) // OUTPUTS: true or false
-//   gulp.src('_dest/css/main.css')
-//   .pipe(removeFiles())
-// })
-// });
-
-gulp.task('concat' , function(){
-  return gulp.src('_dest/css/**/*')
-    .pipe(concat("main.css"))
-    .pipe(gulp.dest('_dest/css/'))
-});
-
-gulp.task('build', function(callback){
-	 runSequence('removeFiles', 'sass2css', ['concat'], callback)
-})
-
 gulp.task('watch' , function(){
-  gulp.watch('_src/sass/**/*' , ['build'])
-  // gulp.watch('_src/sass/**/*' , ['concat'])
+  gulp.watch('_src/sass/**/*' , ['sass2css'])
   gulp.watch('index.html' , browserSync.reload)
-	gulp.watch('_dest/css/main.css' , browserSync.reload)
+	gulp.watch('_dest/css/*' , browserSync.reload)
 	gulp.watch('gulpfile.js' , browserSync.reload)
 });
 gulp.task('default', ['browserSync', 'watch'] ,function(){})
